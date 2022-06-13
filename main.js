3c65e1639042cbc0478131f63104de93524dc514
Jayantika2010 <79147549+jayantika51@users.noreply.github.com>
2022-06-13 16:26:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550, 500);
+    
+    canvas=createCanvas(550, 550);
+    canvas.positio(560, 150);
+    poseNet=ml5.poseNet(video, modelLoaded);
+}
+
+function modelLoaded(){
+    console.log('PoseNet Is Initialized!');
+
+}
+
+function gotPoses(results){
+    if(results.length > 0)
+    {
+console.log(results);
+
+    leftWristX=results[0].pose.leftWrist.x;
+    rightWristX=results[0].pose.rightWrist.x;
+    difference=floor(leftWristX - rightWristX);
+    }
+    
+
+}
+
+function draw(){
+    background('#6C91C2');
+    textSize(10);
+    fill('#FFE787');
+    text('Avengers',50,400)
+}
+
